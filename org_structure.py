766e5e8f0333700c36f172aaff76f229f5aee167
Michael Palmer <MichaelPalmer1@users.noreply.github.com>
2022-02-19 20:34:47 -0500

    fetch policies

--- org_structure.py
+++ org_structure.py
@@ -7,8 +7,14 @@ from utils import build_list
 
 orgs = boto3.client('organizations')
 
+def get_policies(parent_id: str):
+    policies = orgs.list_policies_for_target(TargetId=parent_id, Filter='SERVICE_CONTROL_POLICY')
+    return [
+        policy['Name'] for policy in policies['Policies']
+    ]
+
 def get_children(parent_id: str):
-    organization = {}
+    organization = {'Policies': get_policies(parent_id)}
 
     if parent_id.startswith('r-'):
         organization.update({
@@ -54,7 +60,7 @@ def get_children(parent_id: str):
 
     return organization
 
-def build_diagram(org_structure: dict, show_ids=False, show_accounts=True):
+def build_diagram(org_structure: dict, show_ids=False, show_accounts=True, show_policies=False):
     with Diagram(filename='organization', direction='TB', curvestyle='curved', show=False):
         def render_child(child_structure: dict, parent_node: Node = None):
             # Detect node type
@@ -72,7 +78,9 @@ def build_diagram(org_structure: dict, show_ids=False, show_accounts=True):
             # Build node
             text = child_structure['Name']
             if show_ids:
-                text += f'\n{child_structure["Id"]}'
+                text += f'\n({child_structure["Id"]})'
+            if show_policies:
+                text += f'\n[{", ".join(child_structure["Policies"])}]'
             node = node_type(text)
 
             # Create relationship
@@ -90,45 +98,46 @@ def build_diagram(org_structure: dict, show_ids=False, show_accounts=True):
 
         render_child(org_structure)
 
-def print_structure(org_structure, depth=0, show_ids=False, show_accounts=True):
+def print_structure(org_structure, depth=0, show_ids=False, show_accounts=True, show_policies=False):
         # Build text
-        text = []
+        text = ''
         if org_structure['Type'] == 'ROOT':
-            text.append('\x1B[1m')
+            text += '\x1B[1m'
         elif org_structure['Type'] == 'ORGANIZATIONAL_UNIT':
-            text.append('\x1B[4m')
+            text += '\x1B[4m'
         elif org_structure['Type'] == 'ACCOUNT':
-            text.append('\x1B[3m')
+            text += '\x1B[3m'
 
-        text.append(org_structure['Name'])
-        text.append('\x1B[0m')
+        text += org_structure['Name']
+        text += '\x1B[0m'
 
         if show_ids:
-            text.append(f' ({org_structure["Id"]})')
-        
-        label = ''.join(text)
+            text += f' ({org_structure["Id"]})'        
+        if show_policies:
+            text += f'\t[{", ".join(org_structure["Policies"])}]'
         
         if depth == 0 and org_structure['Type'] == 'ROOT':
-            print(label)
+            print(text)
         elif depth == 1:
-            print('|-- ' + label)
+            print('|-- ' + text)
         else:
-            print('  ' * depth + '|-- ' + label)
+            print('  ' * depth + '|-- ' + text)
 
         # Render child org units
         for org_unit in org_structure.get('OrgUnits', []):
-            print_structure(org_unit, depth=depth + 1, show_ids=show_ids, show_accounts=show_accounts)
+            print_structure(org_unit, depth=depth + 1, show_ids=show_ids, show_accounts=show_accounts, show_policies=show_policies)
         
         # Render child accounts
         if show_accounts:
             for account in org_structure.get('Accounts', []):
-                print_structure(account, depth=depth + 1, show_ids=show_ids, show_accounts=show_accounts)
+                print_structure(account, depth=depth + 1, show_ids=show_ids, show_accounts=show_accounts, show_policies=show_policies)
 
 if __name__ == '__main__':
     # Create parser
     parser = ArgumentParser()
     parser.add_argument('--show-ids', action='store_true', help='Show IDs in the output')
     parser.add_argument('--show-accounts', action='store_true', help='Show accounts in the output')
+    parser.add_argument('--show-policies', action='store_true', help='Show SCPs in the output')
     parser.add_argument('--format', choices=('png', 'json', 'text'), default='text')
     opts = parser.parse_args()
 
@@ -138,7 +147,7 @@ if __name__ == '__main__':
     if opts.format == 'json':
         print(json.dumps(organization, indent=4))
     elif opts.format == 'text':
-        print_structure(organization, show_ids=opts.show_ids, show_accounts=opts.show_accounts) 
+        print_structure(organization, show_ids=opts.show_ids, show_accounts=opts.show_accounts, show_policies=opts.show_policies) 
         print('\nLegend: \x1B[1mROOT\x1B[0m\t\x1B[4mOU\x1B[0m\t\x1B[3mACCOUNT\x1B[0m')
     elif opts.format == 'png':
-        build_diagram(organization, show_ids=opts.show_ids, show_accounts=opts.show_accounts)
+        build_diagram(organization, show_ids=opts.show_ids, show_accounts=opts.show_accounts, show_policies=opts.show_policies)
